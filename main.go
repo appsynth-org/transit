6321dc4d1111511e35c3d1de6e9554a00ce0fc76
Pornanan <ta@appsynth.net>
2023-05-28 19:20:39 +0700

    refactor(Reader): ♻️ refactor spreadsheet reader & clean up

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/appsynth-org/transit/service"
 	"github.com/joho/godotenv"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
 )
 
 type Translation struct {
@@ -39,107 +33,10 @@ func main() {
 		log.Fatalf("Unable to load env config %v", err)
 	}
 
-	cred, err := base64.StdEncoding.DecodeString(os.Getenv("SERVICE_ACCOUNT_BASE64"))
+	_, err = service.ReadSpreadSheet(ctx)
 	if err != nil {
-		log.Fatalf("Unable to load env config %v", err)
+		log.Fatalf("Unable to read spreadsheet %v", err)
 	}
 
-	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.JWTConfigFromJSON(cred, "https://www.googleapis.com/auth/spreadsheets.readonly")
-	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
-	}
-	client := config.Client(ctx)
-
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
-
-	spreadsheetId := os.Getenv("GOOGLE_SHEET_ID")
-	readRange := "SRC"
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve data from sheet: %v", err)
-	}
-
-	if len(resp.Values) == 0 {
-		fmt.Println("No data found.")
-	} else {
-		var groups []Localize
-		currentGroupIndex := -1
-
-		for _, row := range resp.Values {
-			if (len(row)) > 5 {
-				scope := row[0]
-				keyComment := row[1]
-				androidKey := row[2]
-				iosKey := row[3]
-				th := row[4]
-				en := row[5]
-
-				// ignore group comment
-				if len(scope.(string)) > 0 && scope.(string) == "Group comment" {
-					continue
-				}
-
-				// create a new group
-				if (len(scope.(string))) > 0 {
-					groups = append(groups, Localize{
-						GroupName: scope.(string),
-						Keys: []Key{
-							{
-								Comment:    keyComment.(string),
-								AndroidKey: androidKey.(string),
-								IosKey:     iosKey.(string),
-								Translation: Translation{
-									Th: th.(string),
-									En: en.(string),
-								},
-							},
-						},
-					})
-
-					currentGroupIndex = findGroupIndex(scope.(string), groups)
-					continue
-				}
-
-				// if scope is empty, add to current group
-				if (len(scope.(string))) == 0 && currentGroupIndex > -1 {
-					groups[currentGroupIndex].Keys = append(groups[currentGroupIndex].Keys, Key{
-						Comment:     keyComment.(string),
-						AndroidKey:  androidKey.(string),
-						IosKey:      iosKey.(string),
-						Translation: Translation{Th: th.(string), En: en.(string)},
-					})
-					continue
-				}
-
-			}
-		}
-
-		fmt.Printf("Read data from sheet success, Found %v groups 📝", len(groups))
-
-		json, err := json.MarshalIndent(groups, "", "  ")
-		if err != nil {
-			log.Fatalf("Unable to marshal json %v", err)
-		}
-
-		fmt.Println("Writing to output.json")
-		os.WriteFile("output.json", json, 0644)
-	}
-
-}
-
-func findGroupIndex(groupName string, groups []Localize) int {
-	if (len(groupName)) == 0 {
-		return -1
-	}
-
-	for index, g := range groups {
-		if g.GroupName == groupName {
-			return index
-		}
-	}
-	return -1
+	// TODO: write translation to file
 }
